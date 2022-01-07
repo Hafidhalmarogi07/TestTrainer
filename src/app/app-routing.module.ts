79576bdf23efa16e689d4883cb471dcc4bdfc9d9
mikonku <septiawanr318@gmail.com>
2022-01-07 11:51:39 +0700

    push project (belom beres)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import ("./auth/auth.module").then((mod) => mod.AuthModule)
+  },
+  {
+    path: "admin",
+    loadChildren: () => import ("./admin/admin.module").then((mod) => mod.AdminModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/bcasyariah/kkbib/kkbib.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kkbib',
+  templateUrl: './kkbib.component.html',
+  styleUrls: ['./kkbib.component.scss']
+})
+export class KkbibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
